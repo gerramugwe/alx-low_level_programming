bf1ec25f29e0ede4dc9dcfe51992b7a30652a68b
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-14 17:07:22 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stfio.h>
+
+/**
+ * main - prints the lowercase alphabets
+ *
+ * Return always 0 (Sucess)
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+	return (0);
+}
