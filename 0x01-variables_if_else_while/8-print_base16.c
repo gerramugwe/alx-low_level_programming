6e15d4e475922ab9713e2dfe1a03532c4f105241
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-14 18:39:36 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,20 +3,21 @@
 /**
  * main - prints all the numbers of base 16 in lowercase, followed by a new line.
  *
- * Retur: 0 (success)
+ * Retur: 0 (succes)
  */
-
-int main(void)	
+int main(void)
 {
-	char c;
-       	for (c = '0'; c <= '9'; c++)
+	char i;
+
+	for (i = 0; i <= 9; i++)
 	{
-	       	putchar(c);
+		putchar(i);
 	}
-	for (c = 'a'; c <= 'f'; c++)
+	for (i = 'a'; i <= 'f'; i++)
 	{
-		putchar(c);
+		putchar(i);
 	}
-       	putchar('\n');
-       	return 0;
+	putchar('\n');
+	return (0);
 }
+
