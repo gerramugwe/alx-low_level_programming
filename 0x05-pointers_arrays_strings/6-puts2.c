16d0eaaf0f08ea259c61745b8dd19a4da6d39183
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-24 09:02:29 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	i=0;
-	while (*str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(*str[i]);
-		str += 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 
 	_putchar('\n');
