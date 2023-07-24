39084c915bc3617977668f28c38c8555214175ec
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-24 08:33:24 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *puts2 - Prints every other character of a strin
+ * @str: Pointer to the input string
+ *
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+
+	_putchar('\n');
+}
