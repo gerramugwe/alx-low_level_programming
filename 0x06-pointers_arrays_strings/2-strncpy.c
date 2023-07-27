3089f102f5d4b4ad7758ebe6521eb9cec13a6d7f
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-27 07:34:30 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- *
- * _strncpy - Copy a string
- * @dest: The destination value
- * @src: The source value
- * @n: The copy limit
- * Return: char value
+ *_strncpy - function that Copy a string
+ *@dest: The destination value
+ *@src: The source value
+ *@n: The copy limit
+ *Return: char value
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for(i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	for ( ; i < n; i++) 
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strcmp - function that compares two strings
+ *@s1: The first string
+ *@s2: The second string
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+}
+
