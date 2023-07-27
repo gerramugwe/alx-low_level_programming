7f2c0aba947235b11e7a8d1ccdf8df8bf7a486ce
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-27 08:10:28 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,25 @@
  *@s1: The first string
  *@s2: The second string
  *
- *
+ *Return:(s1[i] - s2[i])
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	return (s1[i] - s2[i]);
 }
 
