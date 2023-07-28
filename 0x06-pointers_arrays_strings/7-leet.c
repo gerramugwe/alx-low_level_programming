18e727648858465ba5bbb39bc647356dc0a5c8fe
Gerald Mugwe <gerramugwe@gmail.com>
2023-07-27 19:45:01 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,17 +13,25 @@ char *leet(char *s)
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
 		{
-			else if (s[i] == 'e' || s[i] == 'E')
-				s[i] = '3';
-			else if (s[i] == 'o' || s[i] == 'O')
-				s[i] = '0';
-			else if (s[i] == 't' || s[i] == 'T')
-				s[i] = '7';
-			else if (s[i] == 'l' || s[i] == 'L')
-				s[i] = '1';
+		       	s[i] = '4';
+		}
+		else if (s[i] == 'e' || s[i] == 'E')
 		{
+			s[i] = '3';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
 	}
 	return (s);
 }
