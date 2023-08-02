75944435d225e3dc45cb81301c6804ca107a67c8
Gerald Mugwe <gerramugwe@gmail.com>
2023-08-02 06:55:31 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - Calculate the factorial of a number
+ * @n: The number for which factorial is to be calculated
+ *
+ * Return: Factorial of n, or -1 if n is negative
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+}
