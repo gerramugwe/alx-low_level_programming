94a8bd2c35f3320844c1f1bf60451f411398f7de
Gerald Mugwe <gerramugwe@gmail.com>
2023-08-07 23:51:42 -0700

    concatenates all the arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+  * argstostr - convert the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
+
+char *argstostr(int ac, char **av)
+{
+	int len;
+	int i;
+	char *result;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 0; i < ac; i++)
+	len += strlen(av[i]) + 1;
+	result = malloc(len);
+	if (result == NULL)
+		return (NULL);
+
+	ptr = result;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(ptr, av[i]);
+		ptr += strlen(av[i]);
+		*ptr = '\n';
+		ptr++;
+	}
+	return (result);
+}
