8788349da1dc059eb7ce4b9cd4cd0e74d4d5b43f
Gerald Mugwe <gerramugwe@gmail.com>
2023-09-04 05:16:47 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,25 +7,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int gk, nlot, rwr;
+	int gk, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
 	gk = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (gk == -1)
-		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-	for (nlot = 0; text_content[nlot]; nlot++)
-		;
-	rwr = write(fd, text_content, nlot);
-	if (rwr == -1)
+	if (gk == -1 || w == -1)
 		return (-1);
 
 	close(fd);
+
 	return (1);
 }
+
