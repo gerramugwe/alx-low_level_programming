37127a6cf7f0fd0a095151bf73c97f014d91d423
Gerald Mugwe <gerramugwe@gmail.com>
2023-09-04 05:21:30 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,3 +6,26 @@
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
+{
+	int k, g, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	k = open(filename, O_WRONLY | O_APPEND);
+	w = write(k, text_content, len);
+
+	if (k == -1 || g == -1)
+		return (-1);
+
+	close(k);
+
+	return (1);
+}
+
